3f4b1eeb160a50eb7791beb582c56c27d1179c9a
nastya675756546 <meshcheryakova-nastya@internet.ru>
2024-03-21 16:11:24 +0300

    laba4

--- app/src/main/java/com/example/nmedia/MainActivity.kt
+++ app/src/main/java/com/example/nmedia/MainActivity.kt
@@ -3,9 +3,43 @@ package com.example.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likedByMe: Boolean = false
+)
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нтология начиналась с интенс...",
+            published = "21 мая в 18:36",
+            likedByMe = false
+        )
+        with(binding) {
+            author.text = post.author
+            published.text = post.published
+            content.text = post.content
+            if (post.likedByMe) {
+                like?.setImageResource(R.drawable.ic_liked_24)
+            }
+
+            like?.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                like.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
+                )
+            }
+        }
     }
 }
